8791eee53b4cda4e7e77b7ab57ad905958184df5
idknevo <nevoexists@gmail.com>
2025-09-08 00:27:38 +0300

    fix: better error handling

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@
 
 const config = {
   development: process.env.NODE_ENV === "development",
+  production: process.env.NODE_ENV === "production",
   port: process.env.PORT! || 8000,
   database: process.env.DB_URL!,
   databaseName: process.env.DB_NAME || "natours",
--- src/utils/global-error-handler.ts
+++ src/utils/global-error-handler.ts
@@ -1,16 +1,49 @@
+import config from "@/config/config";
 import { NextFunction, Request, Response } from "express";
 import status from "http-status";
+
+function sendErrorDev(err: any, res: Response) {
+  res.status(err.statusCode || status.INTERNAL_SERVER_ERROR).json({
+    status: {
+      success: false,
+      code: err.statusCode || status.INTERNAL_SERVER_ERROR,
+    },
+    error: err,
+    message: err.message || "Internal Server Error",
+    stack: err.stack,
+  });
+}
+
+function sendErrorProd(err: any, res: Response) {
+  if (err.isOperational) {
+    res.status(err.statusCode || status.INTERNAL_SERVER_ERROR).json({
+      status: {
+        success: false,
+        code: err.statusCode || status.INTERNAL_SERVER_ERROR,
+      },
+      message: err.message || "Internal Server Error",
+    });
+  } else {
+    console.error(err);
+    res.status(status.INTERNAL_SERVER_ERROR).json({
+      status: {
+        success: false,
+        code: status.INTERNAL_SERVER_ERROR,
+      },
+      message: "Something went wrong, Please try again later!",
+    });
+  }
+}
+
 export const globalErrorHanlder = (
   err: any,
   _req: Request,
   res: Response,
   _next: NextFunction,
 ) => {
-  res.status(err.statusCode || status.INTERNAL_SERVER_ERROR).json({
-    status: {
-      success: false,
-      code: err.statusCode || status.INTERNAL_SERVER_ERROR,
-      message: err.message || "Internal Server Error",
-    },
-  });
+  if (config.development) {
+    sendErrorDev(err, res);
+  } else if (config.production) {
+    sendErrorProd(err, res);
+  }
 };

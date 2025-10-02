2dc894d5ee0efcb195a3b83400e75a17364ca231
Ahmed Abdelhafiez <nevoexists@gmail.com>
2025-10-02 20:43:14 +0300

    reviews model

---
+++ src/modules/review/review.model.ts
@@ -0,0 +1,33 @@
+import { InferSchemaType, Model, model, models, Schema } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "A review can't be empty!"],
+    },
+    rating: { type: Number, min: 1, max: 5 },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour"],
+    },
+    user: {
+      type: Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+type reviewType = InferSchemaType<typeof reviewSchema>;
+export const Review: Model<reviewType> =
+  models.Review || model<reviewType>("Review", reviewSchema);
